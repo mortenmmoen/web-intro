bd16191dcc688a794779d1d51e7f3ce418eda2ac
Morten Møretrø Moen <mortem14@student.uia.no>
2018-09-10 16:54:02 +0200

    added picture

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <html lang="no">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1" />
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
 		<title>Hello world</title>
 	</head>
 	<body>
